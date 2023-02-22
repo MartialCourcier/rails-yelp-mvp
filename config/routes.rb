95fe46487eccb869d82ef66a00b15cf32142dd00
Martial Courcier <martialcourcier@sfr.fr>
2023-02-22 18:00:14 +0100

    resto

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+
+  end
+  # get "restaurants", to: "restaurants#index"
+  # get "restaurants/:id", to: "restaurants#show"
+  # get "restaurants/new", to: "restaurants#new"
+  # post "restaurants", to: "restaurants#create"
+  # get "restaurants/:id/edit", to: "restaurants#edit"
+  # get "restaurants/:id/reviews/new"
 end
